c727bc8449684f6712b3c784089f15d7f67e10e3
javiSaugar <javisaugar6@gmail.com>
2025-12-11 17:55:14 +0100

    1º Commit posible WEB
    Tailwinds

---
+++ pruebas/js/prueba.js
@@ -0,0 +1,13 @@
+ 
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'primary-purple': '#8B5CF6',
+                        'primary-blue': '#3B82F6',
+                    },
+                }
+            }
+        }
+
+
