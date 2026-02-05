2af4687d0a70b0cabdbfa6e7a0721e1d1ef9ab2e
javiSaugar <javisaugar6@gmail.com>
2026-02-05 17:41:26 +0100

    ventana mayores - deporte mas iconos
    creación ventanas deporte y mayores
    y logos

--- pruebas/js/prueba.js
+++ pruebas/js/prueba.js
@@ -1,13 +1,1 @@
- 
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        'primary-purple': '#8B5CF6',
-                        'primary-blue': '#3B82F6',
-                    },
-                }
-            }
-        }
-
 
