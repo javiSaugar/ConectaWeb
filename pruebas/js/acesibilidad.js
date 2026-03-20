6fd82738e24e2f2162069f63c9daa45fff2e7498
javiSaugar <javisaugar6@gmail.com>
2026-03-20 09:11:19 +0100

    Añadida Conecta Accesible y apk

--- pruebas/js/acesibilidad.js
+++ pruebas/js/acesibilidad.js
@@ -7,4 +7,35 @@ function mensajeError(mensaje){
     icon: "info",
    theme:"dark"
 });
+
+
 }
+
+
+document.querySelector("#btnIos").addEventListener("click",()=>{
+    mensajeInfo("Estamos trabajando en ello")
+})
+
+function mensajeInfo(mensaje){
+Swal.fire({
+  position: "center",
+  icon: "info",
+  title: mensaje,
+  showConfirmButton: false,
+  timer: 1500
+});
+}
+
+document.querySelector("#btnAndroid").addEventListener("click",()=>{
+        
+   const link = document.createElement("a");
+    link.href = "./Apps/ConectaAccesible.apk"; 
+    link.download = "Conecta.apk";
+    
+
+    link.style.display = "none";
+    document.body.appendChild(link);
+    
+    link.click();
+    document.body.removeChild(link);
+})
